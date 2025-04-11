7778a93af991074efa70b836e77c5f882a8c2147
Lesya <oshynkaren@stud.khpi.ucode-connect.study>
2025-04-12 01:37:33 +0300

    Factorial, Power, Percent, Add and Insert from clipboard

--- js/print.js
+++ js/print.js
@@ -9,6 +9,7 @@ function printNumber(number) {
     if (text.match(/\d+$/) !== null && text.match(/\d+$/)[0] == "0") {
         text = text.replace(/\d+$/, "")
     }
+    if(text.charAt(text.length - 1) == "!") return;
     text += number
     input.setAttribute("value", text)
 }
@@ -26,22 +27,37 @@ function printOperation(operation) {
     let input = document.getElementById("result")
     let text = input.getAttribute("value");
     if (text === null) return;
-    if (text.match(/\d$/) === null) return;
-    if (text === "0") {
-        text = "";
+    //if (text.match(/\d$/) === null) return;
+    // if (text === "0") {
+    //     text = "";
+    // }
+    if (text.match(/[\d\)!]$/) === null && operation !== "√" && operation !== "!") return;
+    if(operation === "^" && text.indexOf("^") !== -1) return;
+    if(operation === "^" && text.indexOf("%") !== -1) return;
+    if (operation === "!" || operation === "^" || operation === "%"){
+        text += operation
+        input.setAttribute("value", text)
+        return
     }
 
-    if (text.match(/[\d\)]$/) === null && operation !== "√") return;
 
     if (operation === "√") {
         text += "√(";
     } else {
-        text += operation;
+        if(text.charAt(0) == "-" && document.querySelector("#content_before").innerHTML != ""){
+            document.querySelector("#content_before").innerHTML += "(" + text + ")" + operation;
+        }else{
+            document.querySelector("#content_before").innerHTML += text + operation;
+        }
+        text = "0";
     }
     input.setAttribute("value", text)
 }
 
 function cleanInput() {
+    if(document.getElementById("result").getAttribute("value") == "0"){
+        document.querySelector("#content_before").innerHTML = ""
+    }
     document.getElementById("result").setAttribute("value", "0")
 }
 
@@ -53,11 +69,61 @@ function printNegative() {
     let number = text.match(/\d+$/)[0]
     if (number == "0") return;
     text = text.replace(/\d+$/, "")
-    input.setAttribute("value", text + "(-" + number + ")")
+    if(text.charAt(0) == "-"){
+        input.setAttribute("value", number)
+        return;
+    }
+    input.setAttribute("value", "-" + number)
+
+    //input.setAttribute("value", text + "(-" + number + ")")
 }
 
+function factorial(n) {
+    return +!n || n * factorial(n - 1);
+}
+function repeatFactorial(n, len) {
+    for(let i = 0; i < len; i++){
+        n = factorial(n);
+    }
+    return n;
+}
+function stringValidator(text) {
+    let str = text.replace(/(\d+)(!+)/g, (_, num, len) => { 
+        const result = repeatFactorial(Number(num), len.length);
+        return result;
+    });
+    str = str.replace(/(\d+\.)?(\d+)(\^)(\d+)(\.\d+)?/g,(_, num1, floatpart1, option , num2, floatpart2) => { 
+        num1 = typeof(num1) == "undefined" ? floatpart1 : num1 + floatpart1;
+        num2 = typeof(floatpart2) == "undefined" ? num2 : num2 + floatpart2;
+        const result = Math.pow(Number(num1), Number(num2))
+        return result.toString();
+    });
+    str = str.replace(/(\d+\.)?(\d+)(\%)(\d+)(\.\d+)?/g,(_, num1, floatpart1, option , num2, floatpart2) => { 
+        num1 = typeof(num1) == "undefined" ? floatpart1 : num1 + floatpart1;
+        num2 = typeof(floatpart2) == "undefined" ? num2 : num2 + floatpart2;
+        let result = Number(num1) * (Number(num2) / 100)
+        return result.toString();
+    });
+    return str;
+}
 function calculate() {
-    let input = document.getElementById("result").getAttribute("value");
+    let text = document.getElementById("result").getAttribute("value")
+    if(text.charAt(0) == "-" && document.querySelector("#content_before").innerHTML != ""){
+        document.querySelector("#content_before").innerHTML += "(" + text + ")";
+    }else{
+        document.querySelector("#content_before").innerHTML += text;
+    }
+    let input = document.querySelector("#content_before").innerHTML;
+
+
+    try{
+        input = stringValidator(input)
+    }catch (error) {
+        document.getElementById("result").setAttribute("value", "Error");
+        return;
+    }
+
+
     if (input === null || input === "0") return;
 
     try {
@@ -89,6 +155,18 @@ function updateHistory() {
         listItem.textContent = entry;
         historyList.appendChild(listItem);
     });
+}
+
+
+function copyToClipboard(){
+    const text = history.join(";");
+    navigator.clipboard.writeText(text)
+}
+
+async function insertFromClipboard(){
+    let text = await navigator.clipboard.readText();
+    history = text.split(";")
+    updateHistory()
 }
 
 function memoryRecall() {
